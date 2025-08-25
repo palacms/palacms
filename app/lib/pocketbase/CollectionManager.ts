c35e89949919fd92b8e496387d964cc221967cee
Jesse Sivonen <j.sivonen@hotmail.com>
2025-08-25 15:41:31 +0300

    Remove checking when lists are loaded
    The method used for checking loading was not reliable. Some lists were
    not updating anymore because they were not related to any component, and
    so staged changes never got cleared for anything.

--- app/lib/pocketbase/CollectionManager.ts
+++ app/lib/pocketbase/CollectionManager.ts
@@ -68,21 +68,7 @@ export const createCollectionManager = () => {
 						})
 					}
 
-					// Wait until all the lists have loaded again, until staged operations are cleared
-					const checkIntervalMs = 100
-					const checkLoaded = () => {
-						for (const list of [...lists.values()]) {
-							if (!list || list.invalidated) {
-								// This list is still loading
-								setTimeout(checkLoaded, checkIntervalMs)
-								return
-							}
-						}
-
-						// All lists loaded, clear staged operations.
-						staged.clear()
-					}
-					setTimeout(checkLoaded, checkIntervalMs)
+					staged.clear()
 				}
 			}
 		},
