778fd2235ce39483beb17b6d78a6f494033e6564
Jesse Sivonen <j.sivonen@hotmail.com>
2025-09-17 15:35:14 +0300

    Redirect to root path for running PalaCMS WASM

--- wasm/web/main.js
+++ wasm/web/main.js
@@ -1,23 +1,27 @@
-const worker = new Worker('/worker.js')
-const init = (sw) => {
-	const channel = new MessageChannel()
-	worker.postMessage('init', [channel.port2])
-	worker.addEventListener('message', (event) => {
-		if (event.data !== 'ready') {
-			return
-		}
+if (location.pathname !== '/') {
+	location.pathname = '/'
+} else {
+	const worker = new Worker('/worker.js')
+	const init = (sw) => {
+		const channel = new MessageChannel()
+		worker.postMessage('init', [channel.port2])
+		worker.addEventListener('message', (event) => {
+			if (event.data !== 'ready') {
+				return
+			}
 
-		sw.postMessage('init', [channel.port1])
-		window.addEventListener('beforeunload', () => {
-			sw.postMessage('exit')
-			worker.terminate()
+			sw.postMessage('init', [channel.port1])
+			window.addEventListener('beforeunload', () => {
+				sw.postMessage('exit')
+				worker.terminate()
+			})
 		})
+	}
+
+	navigator.serviceWorker.register('/sw.js', { scope: '/' }).then((registration) => {
+		if (registration.active) {
+			init(registration.active)
+		}
+		registration.addEventListener('updatefound', () => init(registration.installing))
 	})
 }
-
-navigator.serviceWorker.register('/sw.js', { scope: '/' }).then((registration) => {
-	if (registration.active) {
-		init(registration.active)
-	}
-	registration.addEventListener('updatefound', () => init(registration.installing))
-})
