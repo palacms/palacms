93c5149358388196964d601a47f8ef78b4410bcc
Matthew Morris <accounts@mateomorris.com>
2025-09-22 15:16:53 -0400

    double click field tab to apply to all

--- src/lib/builder/components/Fields/FieldsContent.svelte
+++ src/lib/builder/components/Fields/FieldsContent.svelte
@@ -203,12 +203,12 @@
 					<button
 						data-test-id="field"
 						class:active={active_tab === 'field'}
-						disabled={active_tab === 'field'}
+						ondblclick={() => set_all_tabs('field')}
 						onclick={() => {
 							if ($mod_key_held) {
 								set_all_tabs('field')
 							} else {
-								select_tab(field.id, 'field')
+								if (active_tab !== 'field') select_tab(field.id, 'field')
 							}
 						}}
 					>
@@ -219,12 +219,12 @@
 						data-test-id="entry"
 						style="border-top: 1px solid var(--color-gray-9);"
 						class:active={active_tab === 'entry'}
-						disabled={active_tab === 'entry'}
+						ondblclick={() => set_all_tabs('entry')}
 						onclick={() => {
 							if ($mod_key_held) {
 								set_all_tabs('entry')
 							} else {
-								select_tab(field.id, 'entry')
+								if (active_tab !== 'entry') select_tab(field.id, 'entry')
 							}
 						}}
 					>
