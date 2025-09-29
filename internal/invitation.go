62c2346135d9a4803bbd44a978fc1a27bd16353c
Jesse Sivonen <j.sivonen@hotmail.com>
2025-09-29 14:19:39 +0300

    Send collaborator invitation when assigning site role with information of the site itself

--- internal/invitation.go
+++ internal/invitation.go
@@ -10,20 +10,9 @@ import (
 )
 
 func RegisterEmailInvitation(pb *pocketbase.PocketBase) error {
-	pb.OnRecordAfterCreateSuccess("users").BindFunc(func(event *core.RecordEvent) error {
-		if event.Record.Get("invite") == "pending" {
-			if err := sendInvitation(event); err != nil {
-				event.App.Logger().Error(err.Error())
-			}
-		}
-		return event.Next()
-	})
-
-	pb.OnRecordAfterUpdateSuccess("users").BindFunc(func(event *core.RecordEvent) error {
-		if event.Record.Get("invite") == "pending" {
-			if err := sendInvitation(event); err != nil {
-				event.App.Logger().Error(err.Error())
-			}
+	pb.OnRecordAfterCreateSuccess("site_role_assignments").BindFunc(func(event *core.RecordEvent) error {
+		if err := sendInvitation(event.App, event.Record); err != nil {
+			event.App.Logger().Error(err.Error())
 		}
 		return event.Next()
 	})
@@ -31,38 +20,64 @@ func RegisterEmailInvitation(pb *pocketbase.PocketBase) error {
 	return nil
 }
 
-func sendInvitation(event *core.RecordEvent) error {
-	meta := event.App.Settings().Meta
-	token, err := event.Record.NewPasswordResetToken()
+func sendInvitation(app core.App, roleAssignment *core.Record) error {
+	siteId := roleAssignment.GetString("site")
+	site, err := app.FindRecordById("sites", siteId)
 	if err != nil {
 		return err
 	}
 
+	userId := roleAssignment.GetString("user")
+	user, err := app.FindRecordById("users", userId)
+	if err != nil {
+		return err
+	}
+
+	var html string
+	if user.Get("invite") == "pending" {
+		passwordResetToken, err := user.NewPasswordResetToken()
+		if err != nil {
+			return err
+		}
+
+		html = fmt.Sprintf(
+			"<p>You've been invited to collaborate on %s. Click the link below to create your password.</p>\n<p>\n"+
+				"<a href=\"%s/admin/auth?create=%s\" target=\"_blank\" rel=\"noopener\">Create password</a>\n</p>",
+			site.GetString("name"),
+			"https://"+site.GetString("host"),
+			passwordResetToken,
+		)
+	} else {
+		html = fmt.Sprintf(
+			"<p>You've been invited to collaborate on %[1]s. Below you can find the address for editing the site.</p>\n<p>\n"+
+				"<a href=\"%[2]s/admin\" target=\"_blank\" rel=\"noopener\">%[2]s/admin</a>\n</p>",
+			site.GetString("name"),
+			"https://"+site.GetString("host"),
+		)
+	}
+
+	meta := app.Settings().Meta
 	message := &mailer.Message{
 		From: mail.Address{
 			Address: meta.SenderAddress,
 			Name:    meta.SenderName,
 		},
 		To: []mail.Address{{
-			Address: event.Record.Email(),
+			Address: user.Email(),
 		}},
-		Subject: fmt.Sprintf("You've been invited to collaborate on %s", meta.AppName),
-		HTML: fmt.Sprintf(
-			"<p>You've been invited to collaborate on %s. Click the link below to create your password.</p>\n<p>\n"+
-				"<a href=\"%s/admin/auth?create=%s\" target=\"_blank\" rel=\"noopener\">Create password</a>\n</p>",
-			meta.AppName,
-			meta.AppURL,
-			token,
-		),
+		Subject: fmt.Sprintf("You've been invited to collaborate on %s", site.GetString("name")),
+		HTML:    html,
 	}
 
-	if err := event.App.NewMailClient().Send(message); err != nil {
+	if err := app.NewMailClient().Send(message); err != nil {
 		return err
 	}
 
-	event.Record.Set("invite", "sent")
-	if err := event.App.Save(event.Record); err != nil {
-		return err
+	if user.Get("invite") == "pending" {
+		user.Set("invite", "sent")
+		if err := app.Save(user); err != nil {
+			return err
+		}
 	}
 
 	return nil
