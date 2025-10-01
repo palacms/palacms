50e63ddc491bd0235592f26ad8582f338c570ba7
Matthew Morris <accounts@mateomorris.com>
2025-10-01 04:56:54 -0400

    build: add rich text dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 import { fontFamily } from 'tailwindcss/defaultTheme'
 import tailwindcssAnimate from 'tailwindcss-animate'
+import tailwindTypography from '@tailwindcss/typography'
 
 /** @type {import('tailwindcss').Config} */
 const config = {
@@ -91,7 +92,7 @@ const config = {
 			}
 		}
 	},
-	plugins: [tailwindcssAnimate]
+	plugins: [tailwindcssAnimate, tailwindTypography]
 }
 
 export default config
