8fa01e2c600d5d447d2d926d514169545d796123
Jesse Sivonen <jesse.sivonen@pala.dev>
2025-10-28 18:59:39 +0200

    Handle page type in page field

--- src/lib/builder/field-types/PageFieldField.svelte
+++ src/lib/builder/field-types/PageFieldField.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
 	import { fieldTypes } from '../stores/app'
-	import { PageTypeFields, PageTypes } from '$lib/pocketbase/collections'
+	import { PageTypeEntries, PageTypeFields, PageTypes } from '$lib/pocketbase/collections'
 	import type { Entry } from '$lib/common/models/Entry'
 	import type { Entity } from '$lib/Content.svelte'
 	import { PageEntries } from '$lib/pocketbase/collections'
 	import { setFieldEntries, type FieldValueHandler, type FieldValueMap } from '../components/Fields/FieldsContent.svelte'
 	import type { Field } from '$lib/common/models/Field'
-	import { page_context } from '$lib/builder/stores/context'
+	import { page_context, page_type_context } from '$lib/builder/stores/context'
 
 	const {
-		entity,
 		field,
 		entry,
 		onchange,
@@ -36,23 +35,32 @@
 		return $fieldTypes.find((ft) => ft.id === resolvedField.type)
 	})
 
+	const { value: page_type } = page_type_context.getOr({ value: null })
+	const page_type_fields = $derived(page_type?.fields())
+	const page_type_entries = $derived(page_type?.entries())
+
 	const { value: page } = page_context.getOr({ value: null })
-	const page_type = $derived(page && PageTypes.one(page.page_type))
-	const fields = $derived(page_type?.fields() ?? [])
-	const entries = $derived(page?.entries() ?? [])
+	const page_page_type = $derived(page && PageTypes.one(page.page_type))
+	const page_fields = $derived(page_page_type?.fields())
+	const page_entries = $derived(page?.entries())
+
+	const entity = $derived(page ?? page_type)
+	const fields = $derived(page_fields ?? page_type_fields ?? [])
+	const entries = $derived(page_entries ?? page_type_entries ?? [])
+	const EntriesCollection = $derived(page ? PageEntries : PageTypeEntries)
 
 	// Handle changes to the page field by updating the entry
 	function handleFieldChange(values: FieldValueMap) {
 		setFieldEntries({
 			fields,
 			entries,
-			updateEntry: PageEntries.update,
+			updateEntry: EntriesCollection.update,
 			createEntry: (data) => {
 				if (!page) {
 					throw new Error('No page')
 				}
 
-				return PageEntries.create({ ...data, page: page.id })
+				return EntriesCollection.create({ ...data, page: page.id })
 			},
 			values
 		})
@@ -64,20 +72,20 @@
 		for (const entry of entries) {
 			if (entry.parent === entry_id) {
 				deleteEntryRelatedRecords(entry.id)
-				PageEntries.delete(entry.id)
+				EntriesCollection.delete(entry.id)
 			}
 		}
 	}
 
 	function handleDeleteEntry(entry_id: string) {
 		deleteEntryRelatedRecords(entry_id)
-		PageEntries.delete(entry_id)
+		EntriesCollection.delete(entry_id)
 	}
 </script>
 
-{#if page && resolvedField && fieldType}
+{#if entity && resolvedField && fieldType}
 	{@const SvelteComponent = fieldType.component}
-	<SvelteComponent entity={page} field={{ ...resolvedField, label: field.label }} {entry} {fields} {entries} onchange={handleFieldChange} ondelete={handleDeleteEntry} {level} />
+	<SvelteComponent {entity} field={{ ...resolvedField, label: field.label }} {entry} {fields} {entries} onchange={handleFieldChange} ondelete={handleDeleteEntry} {level} />
 {:else if !field.config?.field}
 	<span>Please configure this field to select a page field.</span>
 {:else if !entry}
