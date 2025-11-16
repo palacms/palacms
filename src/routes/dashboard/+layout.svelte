5ac841f61d325fdd3a7a6b88ca67cc7de2076c47
Matthew Morris <accounts@mateomorris.com>
2025-11-16 08:30:49 -0500

    prevent flash of "Forbidden"

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -52,7 +52,7 @@
 	$effect(() => set_current_user())
 </script>
 
-{#if !$current_user?.serverRole}
+{#if $current_user && !$current_user.serverRole}
 	<div style="display: flex; justify-content: center; align-items: center; height: 100vh; color: white;">Forbidden</div>
 {:else}
 	<Sidebar.Provider>
