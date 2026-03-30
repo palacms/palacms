909130b67d6590aaeffa2325d71295a1f35dbd3f
Sebastian Franco <jhonsfran@gmail.com>
2026-03-30 15:39:51 +0200

    fix: update node 20 again

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -9,7 +9,9 @@ const config = {
     postcss: true
 	}),
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			runtime: 'nodejs20.x'
+		})
 	},
 	vitePlugin: {
 		inspector: false
