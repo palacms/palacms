bbb55de9613179af003d6b84504624f3ccc15292
Sebastian Franco <jhonsfran@gmail.com>
2026-03-30 15:08:37 +0200

    fix: some issues

--- src/lib/supabase/admin.js
+++ src/lib/supabase/admin.js
@@ -1,5 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
-import { PUBLIC_SUPABASE_URL } from '$env/static/public';
-import { PRIVATE_SUPABASE_PRIVATE_KEY } from '$env/static/private';
+import { PRIVATE_SUPABASE_PRIVATE_KEY } from "$env/static/private";
+import { PUBLIC_SUPABASE_URL } from "$env/static/public";
+import { createClient } from "@supabase/supabase-js";
 
-export default createClient(PUBLIC_SUPABASE_URL, PRIVATE_SUPABASE_PRIVATE_KEY)
+export default createClient(PUBLIC_SUPABASE_URL, PRIVATE_SUPABASE_PRIVATE_KEY, {
+	auth: {
+		persistSession: false, // This disables the warning in server environments
+	},
+});
